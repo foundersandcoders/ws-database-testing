6bd7ba68bfc49728db671be4d4a161a62aded5c2
shahenazmonia <shahy.m.93@gmail.com>
2018-11-29 12:45:53 +0200

    create test_db_build file, create user table and insert queries inside it

---
+++ src/database/test_db_build.sql
@@ -0,0 +1,17 @@
+BEGIN;
+
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS computers CASCADE;
+
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    location VARCHAR(30) NOT NULL
+);
+
+INSERT INTO users (name, location) VALUES
+('Matt', 'Moscow');
+('Omar', 'Max');
+
+COMMIT;
